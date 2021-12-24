4fe4983b52b7a7a8a380e157cc9fce22bb0cd770
KinG <44819585+zKinG0D@users.noreply.github.com>
2021-12-24 11:40:59 -0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '𝐾𝑖𝑛𝑔#8563'
+description 'Emprego de Encanador'
+version '1.0.0'
+
+client_scripts {
+    '@vrp/lib/utils.lua',
+    'kclient.lua',
+    'cfg/Config.lua',
+    'cfg/vehConfig.lua'
+}
+
+server_scripts {
+    '@vrp/lib/utils.lua',
+    'kserver.lua',
+    'cfg/Config.lua',
+    'cfg/vehConfig.lua'
+} 
